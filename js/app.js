71a8e24a50d7275a8d0abb3cffa50398490dd058
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2020-03-29 15:48:29 -0300

    Finished

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 function pegarleALaApi(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json)
-        .then(json => console.log(json))
+        .then(response => response.json())
+        .then(json => armadoDelJson(json))
+}
+
+function armadoDelJson(json){
+    var stringJson = ""
+    for (var key in json){
+        stringJson += `${key} = ${json[key]} \n`
+    }
+    
+    document.getElementById("json").innerHTML = stringJson       
+}
+
+
+function seleccionarValorDeSelect(){
+    
+    var select = document.getElementById("opcionesJson") //El <select>
+    var value = select.value //El valor seleccionado
+    
+
+    document.getElementById("json2").innerHTML = value
+
 }
